16d0d2b2eb2b80f76f0786c6ec30ec495927a657
egor <artemyev1003@gmail.com>
2025-11-18 16:08:19 +0900

    improved solution for 2573

--- 2573.py
+++ 2573.py
@@ -1,7 +1,5 @@
-import sys
+from collections import deque
 
-
-sys.setrecursionlimit(10 ** 4)
 
 n, m = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
@@ -10,16 +8,19 @@ grid = [list(map(int, input().split())) for _ in range(n)]
 dx = [0, 1, 0, -1]
 dy = [1, 0, -1, 0]
 
-def dfs(i: int, j: int, visited: list[list[bool]]) -> None:
-    for s in range(4):
-        nx = i + dx[s]
-        ny = j + dy[s]
+def bfs(i: int, j: int, visited: list[list[bool]]) -> None:
+    q = deque([(i, j)])
+    while q:
+        x, y = q.popleft()
+        for s in range(4):
+            nx = x + dx[s]
+            ny = y + dy[s]
 
-        if 0 <= nx < n and 0 <= ny < m and grid[nx][ny] > 0 and not visited[nx][ny]:
-            visited[nx][ny] = True
-            dfs(nx, ny, visited)
+            if 0 <= nx < n and 0 <= ny < m and grid[nx][ny] > 0 and not visited[nx][ny]:
+                visited[nx][ny] = True
+                q.append((nx, ny)) 
 
-def solution():
+def solution(grid: list[list[int]]) -> int:
     year = 0
     while True:
         count = 0
@@ -31,7 +32,7 @@ def solution():
                     count += 1
                     if count > 1:
                         return year
-                    dfs(i, j, visited)
+                    bfs(i, j, visited)
 
         sea = [[True if cell == 0 else False for cell in row] for row in grid]
         for i in range(n):
@@ -50,4 +51,4 @@ def solution():
         if count == 0:
             return 0
 
-print(solution())
+print(solution(grid))
